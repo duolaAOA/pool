4c2c077427f90829785d5ea58638a2d8d314cb4b
YcYo0 <fee9702@gmail.com>
2020-11-08 17:39:53 +0800

    initial

---
+++ pool.go
@@ -0,0 +1,101 @@
+// Package pool implements a pool of net.Conn interfaces to manage and reuse them.
+package pool
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+// Factory is a function to create new connections.
+type Factory func() (net.Conn, error)
+
+// Factory is a function to create new connections.
+type Pool struct {
+	// storage for our net.Conn connections
+	conns chan net.Conn
+
+	// net.Conn generator
+	factory Factory
+
+	// to prevent access to closed channels
+	isDestoryed bool
+}
+
+// New returns a new pool with an initial capacity and maximum capacity.
+// Factory is used when initial capacity is greater then zero to fill the  pool.
+func New(initalCap, maxCap int, factory Factory) (*Pool, error) {
+	if initalCap <= 0 || maxCap <= 0 || initalCap > maxCap {
+		return nil, errors.New("invalid capacity settings")
+	}
+
+	p := &Pool{
+		conns: make(chan net.Conn, maxCap),
+		factory: factory,
+	}
+
+	for i := 0; i < initalCap; i++ {
+		conn, err := factory()
+		if err != nil {
+			return nil, fmt.Errorf("WARNING: factory is not able to fill the pool: %s", err)
+		}
+
+		p.conns <- conn
+	}
+
+	return p, nil
+}
+
+// Get returns a new connection from the pool. After using the connection it
+// should be put back via the Put() method. If there is no new connection
+// available in the pool, a new connection will be created via the Factory()
+// method.
+func (p *Pool) Get() (net.Conn, error) {
+	if p.isDestoryed {
+		return nil, errors.New("pool is destroyed")
+	}
+
+	select {
+	case conn := <- p.conns:
+		return conn, nil
+	default:
+		return p.factory()
+	}
+}
+
+// Put puts a new connection into the pool. If the pool is full, conn is
+// discarded and a warning is output to stderr.
+func (p *Pool) Put(conn net.Conn) error {
+	if p.isDestoryed {
+		return errors.New("pool is destroyed")
+	}
+
+	select {
+	case p.conns <- conn:
+		return nil
+	default:
+		return errors.New("attempt to put into a full pool")
+	}
+}
+
+// Destroy destroys the pool and close all connections. After Destroy() the
+// pool is no longer usable.
+func (p *Pool) Destroy() {
+	if p.isDestoryed {
+		return
+	}
+
+	close(p.conns)
+	p.conns = nil
+	p.factory = nil
+	p.isDestoryed = true
+}
+
+// MaximumCapacity returns the maximum capacity of the pool
+func (p *Pool) MaximumCapacity() int {
+	return cap(p.conns)
+}
+// UsedCapacity returns the used capacity of the pool.
+func (p *Pool) UsedCapacity() int {
+	return len(p.conns)
+}
